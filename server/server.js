e20811612550be66aa7fb28958a4c2eeddd3d659
Onoja Igoche Matthew <onojamatthew59@yahoo.com>
2017-09-16 15:26:59 -0700

    initial commit

--- server/server.js
+++ server/server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 
+const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
-const publicPath = path.resolve(__dirname, 'public');
 const app = express();
 
 app.use(express.static(publicPath));
+
+// app.get('/', (req, res) => {
+//   res.send('/index');
+// });
 
 app.listen(port, () => {
   console.log(`Server is on at ${port}`);
